33894b0a1061b63258686c8d7c80846539307a04
charanpreet13 <c.oberoi@dal.ca>
2019-02-06 18:32:49 +0000

    second commit

--- task1.py
+++ task1.py
@@ -11,3 +11,18 @@ def list_ifname_ip():
                 x=""
                 y=""
                 z=j.split()
+                if(z[0]=="interface"):
+                        interface.append(z[1])
+                if(z[0]=="vlan"):
+                        vlan.append(z[1])
+                if(z[0]=="nameif"):
+                        name.append(z[1])
+                if(z[0]=="ip"):
+                        ip_address.append(z[2])
+                
+        print("Interface:",interface)
+        print("Vlan:",vlan)
+        print("Name:",name)
+        print("Ip address:",ip_address)
+
+list_ifname_ip()
